0c32c1bf670ef28bc9812193e0f9e31e87e48cb6
AryanSingh645 <aryansingh645.developer@gmail.com>
2026-07-18 05:55:27 +0530

    basic signup endpoint done

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,14 +3,45 @@ import jwt from "jsonwebtoken"
 import { authMiddleware } from "./middleware"
 import {JWT_SECRET} from "@repo/backend-common/config"
 import {CreateUserSchema} from "@repo/common/types"
+import {prisma} from "@repo/db/client"
+import bcrypt from "bcrypt"
 
 const app = express()
 
 app.listen(3000)
 
 app.post("/signup", async (req, res) => {
-    // TODO: Db call for registering user
-    const data = CreateUserSchema.safeParse(req.body)
+    try {
+        const data = CreateUserSchema.safeParse(req.body)
+
+        if(!data.success){
+            return res.status(400).json({
+                message: "Invalid data",
+                success: false
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(data.data.password, 10);
+
+        const user = await prisma.user.create({
+            data: {
+                name: data.data?.name,
+                email: data.data.email,
+                password: hashedPassword
+            }
+        })
+
+        return res.status(201).json({
+            message: "User Registered Successfully",
+            success: true,
+            user
+        })
+    } catch (error) {
+        return res.status(400).json({
+            message: "User already exists",
+            success: false
+        })
+    }
 })
 
 app.post("/signin", async (req, res) => {
